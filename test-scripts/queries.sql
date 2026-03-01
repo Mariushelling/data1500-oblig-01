cebaffd944056219881f4e89b284a20b601c66ee
Mariushelling <mariushelling7@gmail.com>
2026-03-01 22:23:28 +0100

    Update queries.sql

--- test-scripts/queries.sql
+++ test-scripts/queries.sql
@@ -2,7 +2,62 @@
 -- TEST-SKRIPT FOR OBLIG 1
 -- ============================================================================
 
+-- Oppgave 5.1
+SELECT * FROM Sykkel;
+
+-- Oppgave 5.2
+SELECT etternavn, fornavn, mobilnummer
+FROM KUNDE
+ORDER BY etternavn ASC;
+
+-- Oppgave 5.3
+SELECT * FROM Sykkel
+WHERE tatt_i_bruk_dato > '2023-43-01';
+
+-- Oppgave 5.4
+SELECT COUNT(*) AS antall_kunder
+FROM Kunde;
+
+-- Oppgave 5.5
+SELECT 
+    k.kunde_id,
+    k.fornavn,
+    k.etternavn,
+    COUNT(u.utleie_id) AS antall_utleier
+FROM Kunde k
+LEFT JOIN Utleie u ON k.kunde_id = u.kunde_id
+GROUP BY k.kunde_id, k.fornavn, k.etternavn
+ORDER BY k.kunde_id;
+
+-- Oppgave 5.6
+SELECT 
+    k.kunde_id,
+    k.fornavn,
+    k.etternavn
+FROM Kunde k
+LEFT JOIN Utleie u ON k.kunde_id = u.kunde_id
+WHERE u.utleie_id IS NULL;
+
+-- Oppgave 5.7
+SELECT 
+    s.sykkel_id,
+    s.tatt_i_bruk_dato
+FROM Sykkel s
+LEFT JOIN Utleie u ON s.sykkel_id = u.sykkel_id
+WHERE u.utleie_id IS NULL;
+
+-- Oppgave 5.8
+SELECT 
+    u.utleie_id,
+    u.sykkel_id,
+    k.fornavn,
+    k.etternavn,
+    u.utlevert_tid
+FROM Utleie u
+JOIN Kunde k ON u.kunde_id = k.kunde_id
+WHERE u.innlevert_tid IS NULL
+  AND u.utlevert_tid < NOW() - INTERVAL '1 day';
+
 -- Kjør med: docker-compose exec postgres psql -h -U admin -d data1500_db -f test-scripts/queries.sql
 
 -- En test med en SQL-spørring mot metadata i PostgreSQL (kan slettes fra din script)
-select nspname as schema_name from pg_catalog.pg_namespace;
